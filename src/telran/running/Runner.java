3709de8603fbbb8dc912b3f3ccdba26e33d70245
ElenaKravtsova <elena@localhost>
2023-04-18 19:24:47 +0300

    CW

--- src/telran/running/Runner.java
+++ src/telran/running/Runner.java
@@ -1,6 +1,7 @@
 package telran.running;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Runner extends Thread {
 	private static final int MIN_SLEEP_TIME = 2;
@@ -8,10 +9,12 @@ public class Runner extends Thread {
 	private static Random random = new Random();
 	int number;
 	int distance;
+	AtomicInteger finished = new AtomicInteger(-1);
 
 	public Runner(int number, int distance) {
 		this.number = number;
 		this.distance = distance;
+		finished = new AtomicInteger(-1);
 	}
 
 	@Override
@@ -23,9 +26,36 @@ public class Runner extends Thread {
 				throw new RuntimeException(e.getMessage());
 			}
 		}
+		finished.compareAndSet(-1, number);
+		
 	}
 
 	private long getSleepTime() {
 		return random.nextLong(MIN_SLEEP_TIME, MAX_SLEEP_TIME);
 	}
 }
+
+//задание
+//не определение победителя
+//должна быть таблица результатов кто на каком месте
+//в таблице дб указано время
+//общий ArrayList потому что надо сохр порядок - этот лист явл объектом синхронизации - он общий
+//в этот лист записать время выполнения этого цикла
+//для таблицы результатов
+
+//нюанс - вопрос где измерять это время (время завершения Среда) - в критической секции или не в ней. Он вр завершения отнять время старта
+//ПОдумать где надо зафикс и опр время пробега этого Среда 
+//если в ней то это мб после блокировки
+//если не в ней - это перед блокировкой - но может получиться что мы ждем пока...?
+
+//точка перехвата в Ранэйбл может быть...
+//ответ - не в ней
+//как бегуны бегут, как измеряется их время
+//время начала у всех дб одинаково
+
+//таблица:
+// место - номерСреда - время его выполнения
+//Place - Thread Number Running Time
+
+//у всех общее время начала (это статическое поле или в классе Рэйс)
+//
--- src/telran/running/Trace.java
+++ src/telran/running/Trace.java
@@ -1,5 +1,6 @@
 package telran.running;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -8,6 +9,7 @@ public class Trace extends Thread {
 	int members;
 	int winner = 0;
 	Set<Runner> runners = new HashSet<>();
+	ArrayList<Runner> results = new ArrayList<>();
 
 	public Trace(int distance, int members) {
 		this.distance = distance;
@@ -22,7 +24,7 @@ public class Trace extends Thread {
 		runners.stream().forEach(Runner::start);
 		while (winner == 0) {
 			runners.stream().forEach(r -> {
-				if (!r.isAlive()) {
+				if (r.finished.get() > 0) {
 					winner = r.number;
 				}
 			});
@@ -31,3 +33,21 @@ public class Trace extends Thread {
 	}
 }
 
+//общий ArrayList потому что надо сохр порядок - этот лист явл объектом синхронизации - он общий
+//в этот лист записать время выполнения этого цикла
+
+//нюанс - вопрос где измерять это время (время завершения Среда) - в критической секции или не в ней. Он вр завершения отнять время старта
+//ПОдумать где надо зафикс и опр время пробега этого Среда 
+//если в ней то это мб после блокировки
+//если не в ней - это перед блокировкой - но может получиться что мы ждем пока...?
+
+//точка перехвата в Ранэйбл может быть...
+//ответ - не в ней
+//как бегуны бегут, как измеряется их время
+//время начала у всех дб одинаково
+
+//таблица:
+//место - номерСреда - время его выполнения
+//Place - Thread Number Running Time
+
+//у всех общее время начала (это статическое поле или в классе Рэйс)
